14433d8f46f6aa0b9ff099649693af9705066246
szucen <634670865@qq.com>
2021-01-24 22:41:04 +0800

    点击标签可跳转至编辑页面

--- src/views/labels.vue
+++ src/views/labels.vue
@@ -1,12 +1,13 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag" v-for="tag in tags" :key="tag.id"
+      :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right"/>
-      </li>
+      </router-link>
 
-    </ol>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -41,8 +42,8 @@
   .tags {
     background-color: white;
     font-size: 16px;
-
-    > li {
+    padding-left: 16px;
+    > .tag {
       min-height: 44px;
       display: flex;
       align-items: center;
