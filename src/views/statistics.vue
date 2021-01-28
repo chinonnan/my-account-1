9976c0862ab6c55ac9d8b42d7ba36b47cb87b9c8
szucen <634670865@qq.com>
2021-01-28 22:27:46 +0800

    删除tabs.vue

--- src/views/statistics.vue
+++ src/views/statistics.vue
@@ -10,14 +10,13 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import Types from '@/components/Money/Types.vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
   import recordTypeList from '@/constants/recordTypeList';
   import intervaList from '@/constants/intervaList';
 
   @Component({
-    components: {Tabs, Types},
+    components: {Tabs,},
   })
   export default class Statistics extends Vue {
     type = '-';
